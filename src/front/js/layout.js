8218c4a3a47a1a987896ccc7489c701a41350a0f
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-08-07 03:09:28 +0000

    Se añadió la barra de navegación responsiva

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/Home.jsx";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
+import { Navbar } from "./component/ Navbar.jsx";
 import { Footer } from "./component/footer";
 
 //create your first component
@@ -13,9 +12,6 @@ const Layout = () => {
   //the basename is used when your project is published in a subdirectory and not in the root of the domain
   // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
   const basename = process.env.BASENAME || "";
-
-  if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "")
-    return <BackendURL />;
 
   return (
     <div>
