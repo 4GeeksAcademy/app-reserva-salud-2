f7c58364f236db0043edf721f36470a560d770e8
Diego Vargas <105869790+diego0o033@users.noreply.github.com>
2024-08-07 18:12:40 +0000

    modelando bd

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Enum
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class RolEnum(Enum):
+    ADMINISTRADOR= "administrador"
+    PACIENTE= "paciente"
+    PROFESIONAL= "profesional"
+
+class Usuario_sistema(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    id_paciente = db.Column(db.String(120), unique=True, nullable=False)
+    id_profesional = db.Column(db.String(80), unique=False, nullable=False)
+    rol=db.Column(db.Enum(RolEnum),nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<Usuario_sistema {self.id_paciente},{self.rol}>'
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "id_paciente": self.paciente,
+            "id_profesional": self.id_profesional,
+            "rol": self.rol,
             # do not serialize the password, its a security breach
         }
+    
