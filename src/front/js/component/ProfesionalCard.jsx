94b237963fd0666aa8e6f73864fc666be4a88482
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-08-11 20:08:43 +0000

    Filtros terminados

--- src/front/js/component/ProfesionalCard.jsx
+++ src/front/js/component/ProfesionalCard.jsx
@@ -1,20 +1,76 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export const ProfesionalCard = ({ profesional }) => {
+  const navigate = useNavigate();
+
+  const renderStars = () => {
+    const filledStars = Math.floor(profesional?.calificacion);
+    const emptyStars = 5 - filledStars;
+
+    const stars = [];
+
+    for (let i = 0; i < filledStars; i++) {
+      stars.push(<i key={i} className="fa-solid fa-star"></i>);
+    }
+
+    for (let i = 0; i < emptyStars; i++) {
+      stars.push(<i key={filledStars + i} className="fa-regular fa-star"></i>);
+    }
+
+    return stars;
+  }
+
+  const departamentos = {
+    artigas: "Artigas",
+    canelones: "Canelones",
+    "cerro-largo": "Cerro Largo",
+    colonia: "Colonia",
+    durazno: "Durazno",
+    flores: "Flores",
+    florida: "Florida",
+    lavalleja: "Lavalleja",
+    maldonado: "Maldonado",
+    montevideo: "Montevideo",
+    paysandu: "Paysandú",
+    "rio-negro": "Río Negro",
+    rivera: "Rivera",
+    rocha: "Rocha",
+    salto: "Salto",
+    "san-jose": "San José",
+    soriano: "Soriano",
+    tacuarembo: "Tacuarembó",
+    "treinta-y-tres": "Treinta y Tres"
+  }
+
+
   return (
-    <div className="card shadow-md m-2">
-      <div className="row">
-        <div className="col-md-4">
-          <img src={profesional?.foto_perfil} className="img-fluid rounded-start" style={{ height: "100%", objectFit: "cover" }} alt="..." />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">{profesional?.usuario.nombre} {profesional?.usuario.apellido}</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+
+    <div className='card bg-primary text-white' onClick={() => navigate(`${profesional.id}`)} style={{ cursor: "pointer" }}>
+      <div className="row align-items-center justify-content-center p-3">
+        <div className="col-5 col-md-4 mx-auto text-center">
+          <img src={profesional.foto_perfil} className='img-fluid rounded-circle' alt="" />
+          <h2 className='text-subtitle text-truncate'>{profesional?.usuario.nombre} {profesional?.usuario.apellido}</h2>
+          <div className="row gap-2">
+            <div className="col">
+              <span className="badge rounded-pill py-2 text-bg-primary">Online</span>
+            </div>
+            <div className="col">
+              <span className="badge rounded-pill py-2 text-bg-success">Presencial</span>
+            </div>
           </div>
+          <p>{renderStars()}</p>
+        </div>
+        <div className="col-7 col-md-8">
+          <h2 className='text-label'>Especialidad:</h2>
+          <p className='text-body text-white'>{profesional?.titulo_habilitante}</p>
+          <h2 className='text-label'>Matrícula:</h2>
+          <p className='text-body text-white'>{profesional?.matricula}</p>
+          <h2 className='text-label'>Ubicación:</h2>
+          <p className='text-body text-white'><i className="fa-solid fa-location-dot"></i> {departamentos[profesional?.departamento]}</p>
         </div>
       </div>
     </div>
+
   )
 }
