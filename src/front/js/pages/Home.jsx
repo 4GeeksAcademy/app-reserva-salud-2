a7ab7f7eaf22ac6c21a9a5edf9f37db4b8eb6942
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-09-01 14:26:00 +0000

    Home renovado

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -48,11 +48,11 @@ export const Home = () => {
         style={{
           backgroundImage: "url(https://c.pxhere.com/images/b3/dd/5be747f89840a4c3c64db3d8fa23-1571981.jpg!d)",
         }}>
-        <div className="hero-overlay bg-opacity-80"></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Reserva tu cita médica en línea, fácil y rápido</h1>
-            <p className="mb-5">
+        <div className="hero-overlay bg-opacity-85"></div>
+        <div className="hero-content text-neutral-content text-center flex-col lg:flex-row">
+          <div className="max-w-lg">
+            <h1 className="mb-5 text-4xl font-bold">Reserva tu cita médica en línea, fácil y rápido</h1>
+            <p className="text-lg mb-5">
               Encuentra al profesional que necesitas y agenda tu consulta en
               cuestión de minutos
             </p>
@@ -62,38 +62,38 @@ export const Home = () => {
       </div>
       {/* Fin Hero */}
 
-      <div className="container flex flex-col items-center gap-y-10 mx-auto px-4 mt-4">
+      <div className="w-full flex flex-col items-center gap-y-12">
         {/* Sección Beneficios */}
-        <section className="mx-auto">
+        <section className="container mx-auto p-4">
+
+          <h2 className="text-2xl font-bold text-gray-700 text-center mb-6">Beneficios</h2>
 
           <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-2">
-            <div className="card bg-base-100 shadow">
-              <div className="card-body text-center">
+
+            <div className="card bg-base-100 w-auto shadow-md">
+              <div className="prose card-body text-center">
                 <i className="fa-solid fa-bolt"></i>
                 <h2 className="card-title justify-center">Rapidez</h2>
                 <p>Reserva en menos de 5 minutos</p>
               </div>
             </div>
-
-            <div className="card bg-base-100 shadow">
-              <div className="card-body text-center">
+            <div className="card bg-base-100 shadow-md">
+              <div className="prose card-body text-center">
                 <i className="fa-solid fa-house"></i>
                 <h2 className="card-title justify-center">Comodidad</h2>
                 <p>Desde la comodidad de tu hogar</p>
               </div>
             </div>
-
-            <div className="card bg-base-100 shadow">
-              <div className="card-body text-center">
+            <div className="card bg-base-100 shadow-md">
+              <div className="prose card-body text-center">
                 <i className="fa-solid fa-circle-nodes"></i>
                 <h2 className="card-title justify-center">Amplia red de profesionales</h2>
                 <p>Encuentra una amplia variedad de profesionales del ámbito de la
                   salud</p>
               </div>
             </div>
-
-            <div className="card bg-base-100 shadow">
-              <div className="card-body text-center">
+            <div className="card bg-base-100 shadow-md">
+              <div className="prose card-body text-center">
                 <i className="fa-solid fa-circle-nodes"></i>
                 <h2 className="card-title justify-center">Confidencialidad</h2>
                 <p>Tus datos están seguros</p>
@@ -102,193 +102,104 @@ export const Home = () => {
 
           </div>
 
-
-          <article className="prose text-center">
-            <h2>Beneficios</h2>
-
-            <div className="d-flex flex-column align-items-center gap-1">
-              <i className="fa-solid fa-bolt"></i>
-              <h3 className="text-subtitle">Rapidez</h3>
-              <p className="text-normal">Reserva en menos de 5 minutos</p>
-            </div>
-
-
-          </article>
-          <div className="container text-primary">
-            <div className="d-flex flex-column flex-md-row align-items-center justify-content-center gap-4">
-
-              <div className="d-flex flex-column align-items-center gap-1">
-                <i className="fa-solid fa-house"></i>
-                <h3 className="text-subtitle">Comodidad</h3>
-                <p className="text-normal">Desde la comodidad de tu hogar</p>
-              </div>
-              <div className="d-flex flex-column align-items-center gap-1">
-                <i className="fa-solid fa-circle-nodes"></i>
-                <h3 className="text-subtitle">Amplia red de profesionales</h3>
-                <p className="text-normal">
-                  Encuentra una amplia variedad de profesionales del ámbito de la
-                  salud
-                </p>
-              </div>
-              <div className="d-flex flex-column align-items-center gap-1">
-                <i className="fa-solid fa-user-shield"></i>
-                <h3 className="text-subtitle">Confidencialidad</h3>
-                <p className="text-normal">Tus datos están seguros</p>
-              </div>
-            </div>
-          </div>
         </section>
 
-        <section className="container-fluid bg-gray py-4 my-5 text-center shadow-md">
-          <div className="container text-primary">
-            <div className="row">
-              <div className="col-md-6 px-3">
-                <h2 className="text-title text-primary mt-5">Misión</h2>
-                <p className="text-normal text-start">Nuestra misión es conectar a profesionales de la salud con sus
-                  pacientes de manera directa, ofreciendo una plataforma sencilla y eficiente que facilita la gestión de
-                  citas, la comunicación y el pago de servicios.</p>
-                <p className="text-normal text-start">Nos enfocamos en brindar una experiencia transparente y
-                  segura, garantizando la protección de los datos personales y asegurando la satisfacción tanto de los
-                  profesionales como de los pacientes.</p>
+
+        {/* Sección Misión y Visión */}
+        <section className="w-full py-8 bg-base-200 text-center">
+          <div className="container mx-auto px-4">
+            <div className="flex justify-center items-center flex-wrap">
+              <div className="w-full md:w-1/2 px-3 order-1">
+                <h2 className="text-2xl font-bold text-primary mt-5">Misión</h2>
+                <p className="text-base text-start mt-4">
+                  Nuestra misión es conectar a profesionales de la salud con sus pacientes de manera directa, ofreciendo una plataforma sencilla y eficiente que facilita la gestión de citas, la comunicación y el pago de servicios.
+                </p>
+                <p className="text-base text-start mt-4">
+                  Nos enfocamos en brindar una experiencia transparente y segura, garantizando la protección de los datos personales y asegurando la satisfacción tanto de los profesionales como de los pacientes.
+                </p>
               </div>
-              <div className="col-md-6">
+              <div className="w-full max-w-lg md:w-1/2 px-3 order-2">
                 <img
-                  className="img-fluid"
-                  style={{ width: '300px', height: '300px' }}
+                  className="w-full"
                   src="https://cdni.iconscout.com/illustration/premium/thumb/man-booking-online-appointment-illustration-download-in-svg-png-gif-file-formats--booked-people-with-appointments-pack-illustrations-3485596.png"
                   alt="Illustration"
                 />
               </div>
-              <div className="col-md-6">
+              <div className="w-full md:w-1/2 px-3 order-3 md:order-4">
+                <h2 className="text-2xl font-bold text-primary mt-5">Visión</h2>
+                <p className="text-base text-start mt-4">
+                  Aspiramos a ser la plataforma líder en la gestión de citas y servicios de salud en línea en Uruguay, reconocida por su eficiencia, facilidad de uso y compromiso con la seguridad y privacidad de nuestros usuarios.
+                </p>
+                <p className="text-base text-start mt-4">
+                  Queremos transformar la manera en que los pacientes acceden a los servicios de salud, promoviendo un modelo más directo, accesible y personalizado, con la posibilidad de acceder a consultas a distancia de forma segura.
+                </p>
+              </div>
+              <div className="w-full max-w-lg md:w-1/2 px-3 order-4 md:order-3">
                 <img
-                  className="img-fluid"
-                  src="https://cdni.iconscout.com/illustration/premium/thumb/woman-schedules-an-appointment-meeting-illustration-download-in-svg-png-gif-file-formats--business-schedule-calendar-planning-time-management-people-with-appointments-pack-illustrations-3485589.png?f=webp  "
+                  className="w-full"
+                  src="https://cdni.iconscout.com/illustration/premium/thumb/woman-schedules-an-appointment-meeting-illustration-download-in-svg-png-gif-file-formats--business-schedule-calendar-planning-time-management-people-with-appointments-pack-illustrations-3485589.png"
                   alt="Illustration"
                 />
               </div>
-              <div className="col-md-6 px-3">
-                <h2 className="text-title text-primary mt-5">Visión</h2>
-                <p className="text-normal text-start">Aspiramos a ser la plataforma líder en la gestión de citas y servicios de
-                  salud en línea en Uruguay, reconocida por su eficiencia, facilidad de uso y
-                  compromiso con la seguridad y privacidad de nuestros usuarios.</p>
-                <p className="text-normal text-start">Queremos
-                  transformar la manera en que los pacientes acceden a los servicios de salud,
-                  promoviendo un modelo más directo, accesible y personalizado, con la posibilidad
-                  de acceder a consultas a distancia de forma segura.</p>
-              </div>
             </div>
           </div>
-        </section >
-
+        </section>
 
         <section className="container">
-          <div className="d-flex flex-column align-items-center py-4">
-            <h2 className="text-title">Preguntas Frecuentes</h2>
+          <div className="flex flex-col items-center py-4">
+            <h2 className="text-2xl font-bold text-gray-700 mb-8">Preguntas Frecuentes</h2>
 
-            <div className="accordion" id="faq">
-              <div className="accordion-item">
-                <h2 className="accordion-header">
-                  <button
-                    className="accordion-button text-label"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#faq-1"
-                    aria-expanded="true"
-                    aria-controls="faq-1"
-                  >
-                    ¿Puedo agendar más de una cita a la vez?
-                  </button>
-                </h2>
-                <div
-                  id="faq-1"
-                  className="accordion-collapse collapse show"
-                  data-bs-parent="#faq"
-                >
-                  <div className="accordion-body text-normal">
-                    Sí, es posible tener reservadas varias citas, siempre y cuando
-                    no coincidan en día y horario. Podrás ver todas tus citas agendadas en tu cuenta.
-                  </div>
+            <div className="join join-vertical w-full">
+              <div className="collapse collapse-arrow join-item bg-base-200">
+                <input type="radio" name="faq" />
+                <div className="collapse-title text-xl font-medium">
+                  ¿Puedo agendar más de una cita a la vez?
+                </div>
+                <div className="collapse-content">
+                  Sí, es posible tener reservadas varias citas, siempre y cuando
+                  no coincidan en día y horario. Podrás ver todas tus citas agendadas en tu cuenta.
                 </div>
               </div>
-              <div className="accordion-item">
-                <h2 className="accordion-header">
-                  <button
-                    className="accordion-button collapsed text-label"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#faq-2"
-                    aria-expanded="false"
-                    aria-controls="faq-2"
-                  >
-                    ¿Puedo cancelar o reprogramar una cita?
-                  </button>
-                </h2>
-                <div
-                  id="faq-2"
-                  className="accordion-collapse collapse"
-                  data-bs-parent="#faq"
-                >
-                  <div className="accordion-body text-normal">
-                    Sí, puedes cancelar o reprogramar tu cita a través de nuestro
-                    sistema de reservas en línea. Solo debes iniciar sesión con tu
-                    cuenta y buscar la cita que deseas modificar.
-                  </div>
+
+              <div className="collapse collapse-arrow join-item bg-base-200">
+                <input type="radio" name="faq" />
+                <div className="collapse-title text-xl font-medium">
+                  ¿Puedo cancelar una cita?
+                </div>
+                <div className="collapse-content">
+                  Sí, puedes cancelar tu cita a través de nuestro
+                  sistema de reservas en línea. Solo debes iniciar sesión con tu
+                  cuenta y buscar la cita que deseas cancelar.
                 </div>
               </div>
-              <div className="accordion-item">
-                <h2 className="accordion-header">
-                  <button
-                    className="accordion-button collapsed text-label"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#faq-3"
-                    aria-expanded="false"
-                    aria-controls="faq-3"
-                  >
-                    ¿Cómo reservo un servicio?
-                  </button>
-                </h2>
-                <div
-                  id="faq-3"
-                  className="accordion-collapse collapse"
-                  data-bs-parent="#faq"
-                >
-                  <div className="accordion-body text-normal">
-                    Es muy sencillo. Solo tienes que seleccionar el o la profesional
-                    o servicio que necesitas, hacer click en reservar y
-                    seguir los pasos indicados en el formulario de reserva.
-                  </div>
+
+              <div className="collapse collapse-arrow join-item bg-base-200">
+                <input type="radio" name="faq" />
+                <div className="collapse-title text-xl font-medium">
+                  ¿Cómo reservo un servicio?
+                </div>
+                <div className="collapse-content">
+                  Es muy sencillo. Solo tienes que seleccionar el o la profesional
+                  o servicio que necesitas, hacer click en reservar y
+                  seguir los pasos indicados en el formulario de reserva.
                 </div>
               </div>
-              <div className="accordion-item">
-                <h2 className="accordion-header">
-                  <button
-                    className="accordion-button collapsed text-label"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#faq-4"
-                    aria-expanded="false"
-                    aria-controls="faq-4"
-                  >
-                    ¿Qué formas de pago aceptan?
-                  </button>
-                </h2>
-                <div
-                  id="faq-4"
-                  className="accordion-collapse collapse"
-                  data-bs-parent="#faq"
-                >
-                  <div className="accordion-body text-normal">
-                    Utilizamos Mercado Pago como pasarela de pago, por lo que
-                    aceptamos una amplia variedad de tarjetas e incluso pagos por
-                    redes de cobranza.
-                  </div>
+
+              <div className="collapse collapse-arrow join-item bg-base-200">
+                <input type="radio" name="faq" />
+                <div className="collapse-title text-xl font-medium">
+                  ¿Qué formas de pago aceptan?
+                </div>
+                <div className="collapse-content">
+                  Utilizamos Mercado Pago como pasarela de pago, por lo que
+                  aceptamos una amplia variedad de tarjetas e incluso pagos por
+                  redes de cobranza.
                 </div>
               </div>
             </div>
           </div>
         </section>
-      </div >
+      </div>
     </>
   );
 };
