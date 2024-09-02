00aed91573f97cd63e78be65e4869ff8c8861b2b
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-09-02 22:56:12 +0000

    Se arregla el precio según tipo de consulta

--- src/front/js/component/Wallet.jsx
+++ src/front/js/component/Wallet.jsx
@@ -1,27 +1,43 @@
 import React, { useContext } from 'react'
 import { Wallet as WalletMP, initMercadoPago } from '@mercadopago/sdk-react'
 import { backendApi } from '../store/flux';
-import { redirect } from 'react-router-dom';
 import { Context } from '../store/appContext';
 
 export const Wallet = ({ appointment }) => {
   const { store } = useContext(Context);
 
+  console.log(appointment)
+
   initMercadoPago('TEST-aa5a7225-3468-4679-81f2-4d313d397c8c', { locale: "es-UY" });
+
+  let items = []
+
+  if (appointment.reservationType === 'remote') {
+    items = [
+      {
+        title: "Cita remota",
+        description: "Reserva de cita remota",
+        quantity: 1,
+        unit_price: 800,
+      },
+    ]
+  } else if (appointment.reservationType === 'presential') {
+    items = [
+      {
+        title: "Cita presencial",
+        description: "Reserva de cita presencial",
+        quantity: 1,
+        unit_price: 1100,
+      },
+    ]
+  }
 
   return (
     <WalletMP
       initialization={{ redirectMode: 'self' }}
       onSubmit={() => {
         const yourRequestBodyHere = {
-          items: [
-            {
-              title: "Cita presencial",
-              description: "Reserva de cita presencial",
-              quantity: 1,
-              unit_price: 1100,
-            },
-          ],
+          items,
           back_urls: {
             success: `${process.env.FRONTEND_URL}/payment-state`,
             failure: `${process.env.FRONTEND_URL}/payment-state`,
