06e9c0f3292b3bedcfe3ddc941486269adde1532
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-09-02 01:38:04 +0000

    Se le agrega la clase prose para que mejore el texto

--- src/front/js/pages/PoliticaDePrivacidad.jsx
+++ src/front/js/pages/PoliticaDePrivacidad.jsx
@@ -1,45 +1,54 @@
 import React from "react";
 
 export const VistaPoliticaPrivacidad = () => {
-    
-    return (
-        <div >
-        <div className="pt-28 pb-16">
-            <h1 className="mb-8 text-5xl font-bold text-center">Política de Privacidad</h1>
-                <p className="text-justify mx-6">En Reserva Salud valoramos y respetamos su privacidad. Esta Política de Privacidad describe cómo recopilamos, utilizamos y protegemos su información personal cuando utiliza nuestra aplicación web para agendar citas con profesionales de la salud registrados en ella. Al utilizar nuestra aplicación, usted acepta las prácticas descritas en esta Política de Privacidad</p>
-                <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">1. Información que recopilamos</h3>
-                    <p className="text-justify mx-6">Recopilamos información personal que usted proporciona al usar nuestra aplicación, como:</p>
-                    <p className="text-justify mx-12">• Nombre completo</p>
-                    <p className="text-justify mx-12">• Correo electrónico</p>
-                    <p className="text-justify mx-12">• Número de teléfono</p>
-                    <p className="text-justify mx-12">• Información de las citas programadas</p>
-                <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">2. Uso de la información</h3>
-                    <p className="text-justify mx-6">Utilizamos su información para:</p>
-                    <p className="text-justify mx-12">• Proveer nuestros servicios</p>
-                    <p className="text-justify mx-12">• Facilitar la programación de citas</p>
-                    <p className="text-justify mx-12">• Enviar recordatorios y notificaciones</p>
-                    <p className="text-justify mx-12">• Gestionar su cuenta</p>
-                <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">3. Protección de la información</h3>
-                    <p className="text-justify mx-6">Nos comprometemos a proteger su información personal mediante el uso de medidas de seguridad adecuadas. Utilizamos protocolos de seguridad estándar de la industria para proteger su información contra acceso no autorizado, alteración, divulgación o destrucción.</p>
-                <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">4. Compartir información con terceros</h3>
-                    <p className="text-justify mx-6">No compartimos su información personal con terceros, excepto en los siguientes casos:</p>
-                    <p className="text-justify mx-12">• Con su consentimiento explícito</p>
-                    <p className="text-justify mx-12">• Para cumplir con leyes y regulaciones aplicables</p>
-                    <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">5. Retención de datos</h3>
-                    <p className="text-justify mx-6">Retenemos su información durante el tiempo necesario para cumplir con los fines descritos en esta política.</p>
-                <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">6. Sus derechos</h3>
-                    <p className="text-justify mx-6">Usted tiene derecho a:</p>
-                    <p className="text-justify mx-12">• Acceder a la información personal que tenemos sobre usted</p>
-                    <p className="text-justify mx-12">• Solicitar la corrección de cualquier información incorrecta o incompleta</p>
-                    <p className="text-justify mx-12">• Solicitar la eliminación de su información personal, bajo ciertas circunstancias</p>
-                    <p className="text-justify mx-12">• Oponerse al procesamiento de su información personal</p>
-                    <p className="text-justify mx-12">• Solicitar la restricción del procesamiento de su información personal</p>
-                    <p className="text-justify mx-6">Para ejercer estos derechos, por favor contáctenos utilizando la información proporcionada al final de esta política</p>
-                <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">7. Cambios a esta Política de Privacidad</h3>
-                    <p className="text-justify mx-6">Podemos actualizar esta política en cualquier momento. Le notificaremos cualquier cambio mediante la publicación de la nueva política en nuestra aplicación.</p>
-                <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">8. Contacto</h3>
-                    <p className="text-justify mx-6">Para cualquier consulta, comuníquese con nosotros a través de alguno de los medios disponibles en la sección Contacto.</p>
-            </div>
-        </div>  
 
-    )};
+    return (
+        <div className="prose max-w-4xl mx-auto">
+            <div className="pt-28 pb-16">
+                <h1>Política de Privacidad</h1>
+                <p>En Reserva Salud valoramos y respetamos su privacidad. Esta Política de Privacidad describe cómo recopilamos, utilizamos y protegemos su información personal cuando utiliza nuestra aplicación web para agendar citas con profesionales de la salud registrados en ella. Al utilizar nuestra aplicación, usted acepta las prácticas descritas en esta Política de Privacidad</p>
+
+                <h2>1. Información que recopilamos</h2>
+                <p>Recopilamos información personal que usted proporciona al usar nuestra aplicación, como:</p>
+                <p>• Nombre completo</p>
+                <p>• Correo electrónico</p>
+                <p>• Número de teléfono</p>
+                <p>• Información de las citas programadas</p>
+
+                <h2>2. Uso de la información</h2>
+                <p>Utilizamos su información para:</p>
+                <p>• Proveer nuestros servicios</p>
+                <p>• Facilitar la programación de citas</p>
+                <p>• Enviar recordatorios y notificaciones</p>
+                <p>• Gestionar su cuenta</p>
+
+                <h2>3. Protección de la información</h2>
+                <p>Nos comprometemos a proteger su información personal mediante el uso de medidas de seguridad adecuadas. Utilizamos protocolos de seguridad estándar de la industria para proteger su información contra acceso no autorizado, alteración, divulgación o destrucción.</p>
+
+                <h2>4. Compartir información con terceros</h2>
+                <p>No compartimos su información personal con terceros, excepto en los siguientes casos:</p>
+                <p>• Con su consentimiento explícito</p>
+                <p>• Para cumplir con leyes y regulaciones aplicables</p>
+
+                <h2>5. Retención de datos</h2>
+                <p>Retenemos su información durante el tiempo necesario para cumplir con los fines descritos en esta política.</p>
+
+                <h2>6. Sus derechos</h2>
+                <p>Usted tiene derecho a:</p>
+                <p>• Acceder a la información personal que tenemos sobre usted</p>
+                <p>• Solicitar la corrección de cualquier información incorrecta o incompleta</p>
+                <p>• Solicitar la eliminación de su información personal, bajo ciertas circunstancias</p>
+                <p>• Oponerse al procesamiento de su información personal</p>
+                <p>• Solicitar la restricción del procesamiento de su información personal</p>
+                <p>Para ejercer estos derechos, por favor contáctenos utilizando la información proporcionada al final de esta política</p>
+
+                <h2>7. Cambios a esta Política de Privacidad</h2>
+                <p>Podemos actualizar esta política en cualquier momento. Le notificaremos cualquier cambio mediante la publicación de la nueva política en nuestra aplicación.</p>
+
+                <h2>8. Contacto</h2>
+                <p>Para cualquier consulta, comuníquese con nosotros a través de alguno de los medios disponibles en la sección Contacto.</p>
+            </div>
+        </div>
+
+    )
+};
