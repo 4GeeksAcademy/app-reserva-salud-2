011ffc9c65256ec4ec0bff867c25635869a78ecf
Diego Vargas <105869790+diego0o033@users.noreply.github.com>
2024-09-02 06:45:06 +0000

    terminado perfil paciente

--- src/front/js/component/TarjetaCitas.jsx
+++ src/front/js/component/TarjetaCitas.jsx
@@ -66,95 +66,108 @@ export const TarjetaCitasUsuario = ({ appointment }) => {
   const isPastAppointment = appointmentDate < currentDate;
   // console.log("isPastAppointment:", isPastAppointment);
   return (
-
-    <div className="col">
-      <div className={`card shadow ${isPastAppointment ? 'bg-light opacity-50' : ''}`}>
-        <div className="card-body">
-          <div className="row justify-content-between">
-            <div className="col text-center">
-              <h5 className="card-title">{appointment?.availability?.professional?.appointment_type}</h5>
-            </div>
-          </div>
-          <div className="row justify-content-center">
-            <div className="col">
-              <p className="card-text text-body"><i className="fa-regular fa-user"></i> {appointment?.availability?.professional?.first_name} {appointment?.availability?.professional?.last_name}</p>
-              <p className="card-text text-body"><i className="fa-regular fa-calendar"></i> {new Date(appointment?.availability?.date).toLocaleDateString('es-UY', { timeZone: 'UTC' })}</p>
-              <p className="card-text text-body"><i className="fa-regular fa-clock"></i> {appointment?.availability?.start_time}</p>
-              {hasCommented && <span className="badge bg-success w-100 mb-3">Comentario realizado</span>}
-            </div>
-            <div className="col">
-              <button className={`btn w-100  ${isPastAppointment ? 'btn-dark' : 'btn-danger'}`}
-                onClick={cancelAppointment}
-                disabled={isPastAppointment}
-              >
-                Cancelar Reserva
-              </button>
-            </div>
-          </div>
-          {/* Contenedor del botón "Comentar" */}
-          {isPastAppointment && (
-            <div className="row mt-2">
-              <div className="col">
-                <button
-                  className="btn btn-primary w-100"
-                  onClick={() => {
-                    if (!hasCommented) {
-                      setShowModal(true);
-                    } else {
-                      toast.info("El servicio ya ha sido comentado.");
-                    }
-                  }}
-                  disabled={hasCommented} // Deshabilitar si ya se ha comentado
-                  style={{ opacity: hasCommented ? 0.5 : 1 }}
-                >
-                  Comentar Servicio
-                </button>
-              </div>
-            </div>
-          )}
+    <>
+   {/* Contenedor de la tarjeta */}
+<div className="col">
+  <div className={`card shadow bg-primary ${isPastAppointment ? 'bg-light opacity-50' : ''}`}>
+    <div className="card-body p-2 mt-1 mb-1">
+      <div className="row justify-content-between">
+        <div className="col text-center">
+          <h5 className="card-title">{appointment?.availability?.professional?.appointment_type}</h5>
         </div>
       </div>
-
-      {/* Modal para realizar el comentario */}
-      {showModal && (
-        <div className="modal show d-block" tabIndex="-1">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Agregar Comentario</h5>
-                <button type="button" className="btn-close" onClick={() => setShowModal(false)}></button>
-              </div>
-              <div className="modal-body">
-                <textarea
-                  className="form-control"
-                  rows="4"
-                  value={comment}
-                  onChange={(e) => setComment(e.target.value)}
-                  placeholder="Escribe tu comentario aquí..."
-                ></textarea>
-                <h6>Agregar puntuación del servicio</h6>
-                <input
-                  type="number"
-                  className="form-control"
-                  value={score}
-                  onChange={(e) => setScore(e.target.value)}
-                  placeholder="Ingresa un score del 1 al 5"
-                  min="1"
-                  max="5"
-                />
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" onClick={() => setShowModal(false)}>
-                  Cerrar
-                </button>
-                <button type="button" className="btn btn-primary" onClick={handleCommentSubmit}>
-                  Enviar Comentario
-                </button>
-              </div>
-            </div>
-          </div>
+      <div className="row justify-content-center">
+        <div className="col">
+          <p className="card-text text-body">
+            <h3 className="text-white">Apellido y nombre especialista:</h3>
+            <i className="fa-regular fa-user"></i> {appointment?.availability?.professional?.first_name}{' '}
+            {appointment?.availability?.professional?.last_name}
+          </p>
+          <p className="card-text text-body">
+          <h3 className="text-white">Día de la cita:</h3>
+            <i className="fa-regular fa-calendar"></i>{' '}
+            {new Date(appointment?.availability?.date).toLocaleDateString('es-UY', { timeZone: 'UTC' })}
+          </p>
+          <p className="card-text text-body">
+          <h3 className="text-white">Hora de la cita:</h3>
+            <i className="fa-regular fa-clock"></i> {appointment?.availability?.start_time}
+          </p>
+          {hasCommented && <span className="badge bg-success w-100 mb-3">Comentario realizado</span>}
         </div>
-      )}
+        <div className="col">
+          <button
+            className={`btn btn bg-blue-400 w-full text-white ${isPastAppointment ? 'btn-primary' : 'bg-blue-400'}`}
+            onClick={cancelAppointment}
+            disabled={isPastAppointment}
+          >
+            Cancelar Reserva
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
+
+  {/* Contenedor del botón modal fuera de la card */}
+  {isPastAppointment && (
+    <div className="mt-2">
+      <button
+        className={`btn bg-blue-500 w-full ${hasCommented ? 'opacity-50' : ''}`}
+        onClick={() => {
+          if (!hasCommented) {
+            document.getElementById('comment_modal').showModal();
+          } else {
+            toast.info('El servicio ya ha sido comentado.');
+          }
+        }}
+        disabled={hasCommented}
+      >
+        Comentar Servicio
+      </button>
+    </div>
+  )}
+</div>
+
+{/* Modal de DaisyUI */}
+<dialog id="comment_modal" className="modal modal-bottom sm:modal-middle">
+  <form method="dialog" className="modal-box">
+    <h3 className="font-bold text-lg">Agregar Comentario</h3>
+
+    <div className="py-4">
+      {/* Textarea para el comentario */}
+      <textarea
+        className="textarea textarea-bordered w-full mb-4"
+        rows="4"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        placeholder="Escribe tu comentario aquí..."
+      ></textarea>
+
+      {/* Input para la puntuación */}
+      <h6 className="font-medium">Agregar puntuación del servicio</h6>
+      <input
+        type="number"
+        className="input input-bordered w-full mb-4"
+        value={score}
+        onChange={(e) => setScore(e.target.value)}
+        placeholder="Ingresa un score del 1 al 5"
+        min="1"
+        max="5"
+      />
+    </div>
+
+    <div className="modal-action">
+      {/* Botón para cerrar el modal */}
+      <button type="button" className="btn btn-secondary" onClick={() => document.getElementById('comment_modal').close()}>
+        Cerrar
+      </button>
+
+      {/* Botón para enviar el comentario */}
+      <button type="button" className="btn btn-primary" onClick={handleCommentSubmit}>
+        Enviar Comentario
+      </button>
+    </div>
+  </form>
+</dialog>
+</>
   );
 };
