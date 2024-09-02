360d3d43f338c905a48d681d2803f68cce019645
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-09-02 01:37:02 +0000

    Se le pone la clase prose para que mejore el estilo de texto

--- src/front/js/component/terminos-y-condiciones.jsx
+++ src/front/js/component/terminos-y-condiciones.jsx
@@ -3,36 +3,45 @@ import "../../styles/home.css";
 
 export const TerminosYCondiciones = () => {
     return (
-        <div >
+        <div className="prose max-w-4xl mx-auto" >
             <div className="pt-28 pb-16">
                 <h1 className="mb-8 text-5xl font-bold text-center">Términos y condiciones</h1>
-                    <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">1. Aceptación de los términos</h3>
-                        <p className="text-justify mx-6">Al utilizar esta aplicación, usted acepta los términos y condiciones aquí establecidos. Estos términos pueden ser modificados en cualquier momento, por lo que le recomendamos revisarlos periódicamente.</p>
-                    <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">2. Uso de la aplicación</h3>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• Propósito:</span> Esta aplicación está diseñada para facilitar la reserva de citas médicas.</p>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• Responsabilidad del Usuario:</span> El usuario es responsable de proporcionar información precisa y actualizada al momento de realizar una reserva.</p>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• Prohibido:</span> Está prohibido cualquier uso que pueda dañar, deshabilitar o interferir en el funcionamiento de la aplicación.</p>
-                    <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">3. Servicios médicos</h3>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• No somos proveedores:</span> La aplicación es una plataforma que conecta a pacientes con proveedores de servicios médicos. No somos responsables por la calidad de los servicios prestados por los proveedores.</p>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• Información médica:</span> La información proporcionada por la aplicación tiene fines informativos y no sustituye el consejo médico profesional.</p>
-                    <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">4. Privacidad</h3>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• Recopilación de datos:</span> Recopilamos información personal para poder prestar nuestros servicios.</p>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• Uso de datos:</span> Utilizamos sus datos para procesar reservas, enviarle notificaciones y mejorar nuestros servicios.</p>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• Seguridad:</span> Implementamos medidas de seguridad para proteger sus datos personales.</p>
-                    <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">5. Pagos</h3>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• Formas de pago:</span> Aceptamos las siguientes formas de pago: Tarjetas de crédito, tarjetas de débito, redes de cobranza (Abitab, RedPagos).</p>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• Reembolsos:</span> Las políticas de reembolso se detallan en nuestra sección de preguntas frecuentes.</p>
-                    <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">6. Cancelación y reprogramación</h3>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• Cancelación:</span> Puede cancelar su cita dentro de un plazo determinado.</p>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• Reprogramación:</span> Puede reprogramar su cita con cierta antelación.</p>
-                        <p className="text-justify mx-6"> <span className="font-semibold">• No presentarse:</span> Si no se presenta a su cita, podrían aplicarse cargos.</p>
-                    <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">7. Limitación de responsabilidad</h3>
-                        <p className="text-justify mx-6">No seremos responsables por ningún daño directo, indirecto, incidental o consecuente derivado del uso o la imposibilidad de usar esta aplicación.</p>
-                    <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">8. Ley aplicable</h3>
-                        <p className="text-justify mx-6">Estos términos se rigen por las leyes de Uruguay.</p>
-                    <h3 className="text-lg text-gray-500 font-bold mt-6 mx-6">9. Contacto</h3>
-                        <p className="text-justify mx-6">Para cualquier consulta, comuníquese con nosotros a través de alguno de los medios disponibles en la sección Contacto.</p>
-                </div>
-            </div>  
+
+                <h2>1. Aceptación de los términos</h2>
+                <p>Al utilizar esta aplicación, usted acepta los términos y condiciones aquí establecidos. Estos términos pueden ser modificados en cualquier momento, por lo que le recomendamos revisarlos periódicamente.</p>
+
+                <h2>2. Uso de la aplicación</h2>
+                <p> <strong>• Propósito:</strong> Esta aplicación está diseñada para facilitar la reserva de citas médicas.</p>
+                <p> <strong>• Responsabilidad del Usuario:</strong> El usuario es responsable de proporcionar información precisa y actualizada al momento de realizar una reserva.</p>
+                <p> <strong>• Prohibido:</strong> Está prohibido cualquier uso que pueda dañar, deshabilitar o interferir en el funcionamiento de la aplicación.</p>
+
+                <h2>3. Servicios médicos</h2>
+                <p> <strong>• No somos proveedores:</strong> La aplicación es una plataforma que conecta a pacientes con proveedores de servicios médicos. No somos responsables por la calidad de los servicios prestados por los proveedores.</p>
+                <p> <strong>• Información médica:</strong> La información proporcionada por la aplicación tiene fines informativos y no sustituye el consejo médico profesional.</p>
+
+                <h2>4. Privacidad</h2>
+                <p> <strong>• Recopilación de datos:</strong> Recopilamos información personal para poder prestar nuestros servicios.</p>
+                <p> <strong>• Uso de datos:</strong> Utilizamos sus datos para procesar reservas, enviarle notificaciones y mejorar nuestros servicios.</p>
+                <p> <strong>• Seguridad:</strong> Implementamos medidas de seguridad para proteger sus datos personales.</p>
+
+                <h2>5. Pagos</h2>
+                <p> <strong>• Formas de pago:</strong> Aceptamos las siguientes formas de pago: Tarjetas de crédito, tarjetas de débito, redes de cobranza (Abitab, RedPagos).</p>
+                <p> <strong>• Reembolsos:</strong> Las políticas de reembolso se detallan en nuestra sección de preguntas frecuentes.</p>
+
+                <h2>6. Cancelación y reprogramación</h2>
+                <p> <strong>• Cancelación:</strong> Puede cancelar su cita dentro de un plazo determinado.</p>
+                <p> <strong>• Reprogramación:</strong> Puede reprogramar su cita con cierta antelación.</p>
+                <p> <strong>• No presentarse:</strong> Si no se presenta a su cita, podrían aplicarse cargos.</p>
+
+                <h2>7. Limitación de responsabilidad</h2>
+                <p>No seremos responsables por ningún daño directo, indirecto, incidental o consecuente derivado del uso o la imposibilidad de usar esta aplicación.</p>
+
+                <h2>8. Ley aplicable</h2>
+                <p>Estos términos se rigen por las leyes de Uruguay.</p>
+
+                <h2>9. Contacto</h2>
+                <p>Para cualquier consulta, comuníquese con nosotros a través de alguno de los medios disponibles en la sección Contacto.</p>
+            </div>
+        </div>
     )
 }
