bdb73abf0f19ea2f1f439c3d9142825803fb4955
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-09-02 16:50:30 +0000

    Arreglos en la estructura del perfil profesional

--- src/front/js/pages/PerfilProfesionalProvisorio.jsx
+++ src/front/js/pages/PerfilProfesionalProvisorio.jsx
@@ -4,25 +4,49 @@ import { Context } from "../store/appContext.js";
 
 export const VistaPerfilProfesional = () => {
     const [professionalAppointments, setProfessionalAppointments] = useState([]);
-    const { actions } = useContext(Context);
+    const [loading, setLoading] = useState(true);
+    const { actions, store } = useContext(Context);
 
     useEffect(() => {
         const getProfessionalAppointments = async () => {
             const response = await actions.getProfessionalAppointments()
             setProfessionalAppointments(response.data)
+            setLoading(false)
         }
         getProfessionalAppointments();
     }, []);
 
-    return (
-        <div className="contenido pt-28">
-            <h1 className="mb-8 text-4xl font-bold text-center">Bienvenido</h1>
-            <div className="d-flex justify-content-center">
-                <h3 className="text-2xl mx-8 w-75">Pacientes agendados</h3>
+    if (loading) {
+        return (
+            <div className="container mx-auto min-h-screen pt-20">
+                <div className="flex justify-center mb-8">
+                    <div className="skeleton h-10 w-96"></div>
+                </div>
+                <div className="flex justify-center">
+                    <div className="skeleton h-6 w-64"></div>
+                </div>
+                <div className="grid sm:grid-cols-2 gap-6 mt-10">
+                    <div className="skeleton h-40 w-full"></div>
+                    <div className="skeleton h-40 w-full"></div>
+                    <div className="skeleton h-40 w-full"></div>
+                    <div className="skeleton h-40 w-full"></div>
+                </div>
             </div>
-            <div className="grid sm:grid-cols-2 gap-6 m-8">
-                {professionalAppointments.map((appointment) => (
-                    <TarjetaCitasProfesional key={appointment.id} appointment={appointment} />))}
+        )
+    }
+
+    return (
+        <div className="container mx-auto min-h-screen pt-20">
+            <h2 className="mb-8 text-4xl font-bold text-center">Bienvenido {store?.currentProfessional?.first_name} {store?.currentProfessional?.last_name}</h2>
+            <div className="flex justify-center">
+                <h3 className="text-2xl font-medium">Pacientes agendados</h3>
+            </div>
+            <div className="grid sm:grid-cols-2 gap-6 mt-10">
+                {
+                    professionalAppointments?.map((appointment) => (
+                        <TarjetaCitasProfesional key={appointment.id} appointment={appointment} />
+                    ))
+                }
             </div>
         </div>
     )
