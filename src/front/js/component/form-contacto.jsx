733d7ec580464625b5dd12d18192fdb708aee381
Victoria Martínez <victoriamartinezruiz23@gmail.com>
2024-09-02 03:47:40 +0000

    Avance vista contacto

--- src/front/js/component/form-contacto.jsx
+++ src/front/js/component/form-contacto.jsx
@@ -20,40 +20,83 @@ export const FormContacto = () => {
     };
 
     return (
-        <div className="contenido">
-            <h1 className="text-center text-title text-primary">Contacto</h1>
-            <div className="row">
-                <div className="col-sm-12 col-md-6 mx-3 p-5">
-                    <p className="text-label text-secondary fw-semibold">Contáctese a través de este formulario:</p>
-                    <form onSubmit={enviarEmail}>
-                        <div className="mb-3">
-                            <label className="text-label">Nombre</label>
-                            <input type="text" className="form-control" id="nombre" name="nombre" aria-describedby="nombre" />
+        <div >
+        <div className="hero min-h-screen" style={{
+            backgroundImage: "url(https://www.sivsa.com/site/wp-content/uploads/2018/04/stethoscope-2617700_1280-1024x682.jpg)",
+        }}>
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form className="card-body">
+                        <div className="text-center lg:text-left">
+                            <h1 className="text-5xl font-bold">Contáctate con nosotros</h1>
+                            <p className="py-6">
+                                <p className="text-label text-secondary fw-semibold">Otras formas de contactarnos</p>
+                                <p className="text-primary text-normal fw-bold mb-0"><i className="fa-solid fa-envelope"></i> Email:</p>
+                                <p className="text-label">contacto@reservasalud.com</p>
+                                <p className="text-primary text-normal fw-bold mb-0"><i className="fa-brands fa-square-instagram"></i> Instagram</p>
+                                <p className="text-label">ReservaSalud</p>
+                                <p className="text-primary text-normal fw-bold mb-0"><i className="fa-brands fa-linkedin"></i> Linkedin</p>
+                                <p className="text-label">Reserva Salud</p>
+                            </p>
                         </div>
-                        <div className="mb-3">
-                            <label className="text-label">Correo Electrónico</label>
-                            <input type="email" className="form-control" id="correo_electronico" name="correo_electronico" />
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" required />
                         </div>
-                        <div className="mb-3">
-                            <label className="text-label">Mensaje</label>
-                            <textarea id="mensaje" name="mensaje" rows="4" cols="50" className="form-control" />
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
                         </div>
-                        <button type="submit" className="btn btn-primary">Enviar</button>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
                     </form>
-
-                </div>
-                <div className="col container">
-                    <div className="bg-gray p-3 m-5 h-75" >
-                        <p className="text-label text-secondary fw-semibold">Otras formas de contactarnos</p>
-                        <p className="text-primary text-normal fw-bold mb-0"><i className="fa-solid fa-envelope"></i> Email:</p>
-                        <p className="text-label">contacto@reservasalud.com</p>
-                        <p className="text-primary text-normal fw-bold mb-0"><i className="fa-brands fa-square-instagram"></i> Instagram</p>
-                        <p className="text-label">ReservaSalud</p>
-                        <p className="text-primary text-normal fw-bold mb-0"><i className="fa-brands fa-linkedin"></i> Linkedin</p>
-                        <p className="text-label">Reserva Salud</p>
-                    </div>
                 </div>
             </div>
         </div>
+        </div>
     );
 };
+
+
+// <div className="pt-28 pb-16">
+        //     <h1 className="text-center text-title text-primary">Contacto</h1>
+        //     <div className="row">
+        //         <div className="col-sm-12 col-md-6 mx-3 p-5">
+        //             <p className="text-label text-secondary fw-semibold">Contáctese a través de este formulario:</p>
+        //             <form onSubmit={enviarEmail}>
+        //                 <div className="mb-3">
+        //                     <label className="text-label">Nombre</label>
+        //                     <input type="text" className="form-control" id="nombre" name="nombre" aria-describedby="nombre" />
+        //                 </div>
+        //                 <div className="mb-3">
+        //                     <label className="text-label">Correo Electrónico</label>
+        //                     <input type="email" className="form-control" id="correo_electronico" name="correo_electronico" />
+        //                 </div>
+        //                 <div className="mb-3">
+        //                     <label className="text-label">Mensaje</label>
+        //                     <textarea id="mensaje" name="mensaje" rows="4" cols="50" className="form-control" />
+        //                 </div>
+        //                 <button type="submit" className="btn btn-primary">Enviar</button>
+        //             </form>
+        //         </div>
+        //         <div className="col container">
+        //             <div className="bg-gray p-3 m-5 h-75" >
+        //                 <p className="text-label text-secondary fw-semibold">Otras formas de contactarnos</p>
+        //                 <p className="text-primary text-normal fw-bold mb-0"><i className="fa-solid fa-envelope"></i> Email:</p>
+        //                 <p className="text-label">contacto@reservasalud.com</p>
+        //                 <p className="text-primary text-normal fw-bold mb-0"><i className="fa-brands fa-square-instagram"></i> Instagram</p>
+        //                 <p className="text-label">ReservaSalud</p>
+        //                 <p className="text-primary text-normal fw-bold mb-0"><i className="fa-brands fa-linkedin"></i> Linkedin</p>
+        //                 <p className="text-label">Reserva Salud</p>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
